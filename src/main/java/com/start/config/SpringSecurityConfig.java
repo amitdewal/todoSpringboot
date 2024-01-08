96b32181e6a2156e0717c2bf003871d36797ede5
Amit Dewal <daredewal007@gmail.com>
2024-01-08 23:31:21 +0530

    spring security is applied ,basic and form based security

---
+++ src/main/java/com/start/config/SpringSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.start.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((authorize) -> {
+			authorize.anyRequest().authenticated();
+		}).httpBasic(Customizer.withDefaults());
+		return httpSecurity.build();
+
+	}
+
+}
