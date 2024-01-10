01d0123b348ba5018f10d44d42738165415aaa3b
Amit Dewal <daredewal007@gmail.com>
2024-01-11 00:11:32 +0530

    implemen of the spring security using in memory by UserDetails Ssrvice and SecurityFilterChain

--- src/main/java/com/start/config/SpringSecurityConfig.java
+++ src/main/java/com/start/config/SpringSecurityConfig.java
@@ -2,20 +2,59 @@ package com.start.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+	
+	//password encoder 
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((authorize) -> {
+			authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+			authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+			authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+			authorize.requestMatchers(HttpMethod.GET,"/api/**").hasAnyRole("ADMIN","USER");
+			authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN","USER");
+			authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
 			authorize.anyRequest().authenticated();
 		}).httpBasic(Customizer.withDefaults());
 		return httpSecurity.build();
 
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails amit = User.builder()
+								.username("amit")
+								.password(passwordEncoder().encode("password"))
+								.roles("USER")
+								.build();
+		
+		UserDetails admin = User.builder()
+								.username("admin")
+								.password(passwordEncoder().encode("admin"))
+								.roles("ADMIN")
+								.build();
+		
+		return new InMemoryUserDetailsManager(amit, admin);
+
+		
+		
 	}
 
 }
